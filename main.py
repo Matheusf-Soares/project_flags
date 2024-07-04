7d1c6886d4e77e2fc1497160bbdff7b54819464b
Matheusf-Soares <matheusfps27@icloud.com>
2024-07-04 16:46:51 -0300

    Inicio da Análise Exploratória de Dados

---
+++ main.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+flags = pd.read_csv('flags.csv')
+
+print('head()')
+print(flags.head())
+print('-' * 90)
+
+print('tail()')
+print(flags.tail())
+print('-' * 90)
+
+verde = flags['green']
+amarelo = flags['gold']
+azul = flags['blue']
+branco = flags['white']
+soma = verde + amarelo + azul + branco
+
+tem_todas = (soma == 4)
+tem_todas
+
+print('Países com verde, amarelo, azul e branco')
+print(flags[tem_todas.values]['name'])
+
+i = 0
+for c in flags.columns:
+    i += 1
+    att = flags[c]
+    att_dtype = att.dtype
+    att_tam_dominio = att.unique().size
+
+    att_tem_nulo = any(att.isnull())
+    if (att_tam_dominio < 8):
+        print(f'({str(i)}) atributo: ', c, '\t', 
+              'dtype: ', att_dtype, '\t',
+              'nulos: ', att_tem_nulo, '\t',
+              'domínio: ', att.unique())
+    else:
+        if (att_dtype == 'object'):
+            print(f'({str(i)}) atributo: ', c, '\t', 
+              'dtype: ', att_dtype, '\t',
+              'nulos: ', att_tem_nulo, '\t',
+              'domínio (primeior elementos): ', att.unique()[:8])
+        else:
+            print(f'({str(i)}) atributo: ', c, '\t', 
+              'dtype: ', att_dtype, '\t',
+              'nulos: ', att.min(), '\t',
+              'min: ', att.min(), '\t',
+              'max: ', att.max(), '\t',
+              'media: ', round(att.mean(), 2), '\t',
+              'd.p.: ', round(att.std(), 2))
